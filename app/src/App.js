7d67f690b2c3580d908d4f690faf2a830ec6c80a
Johanna Lynn Lahti <johanna.lynn.lahti@gmail.com>
2018-08-18 07:18:18 +0200

    Fetch tournaments to app, add bootstrap, set base path of api

--- app/src/App.js
+++ app/src/App.js
@@ -3,16 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    state = {
+        isLoading: true,
+        tournaments: []
+    };
+
+    async componentDidMount() {
+        const response = await fetch('/api/tournaments')
+        const body = await response.json();
+        console.log(body._embedded.tournaments);
+        const list = body._embedded.tournaments;
+        this.setState({ tournaments: list, isLoading: false });
+    }
+
   render() {
+        const { tournaments, isLoading } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <h2>List of tournaments</h2>
+            { tournaments.map (tournament =>
+                <div key={tournament.name}>
+                    {tournament.name}
+                </div>
+            )}
+        </div>
       </div>
     );
   }
